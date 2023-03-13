20a035921ba3c963b8be8f4754d7684daaff130b
ferbie10 <j.j.uding.festus@gmail.com>
2023-03-13 12:55:13 +0000

    modified:   LSTM_Model.py 	modified:   main.py 	modified:   stock_data.py

--- main.py
+++ main.py
@@ -15,19 +15,30 @@ def main():
     start_year = today.year - years_past
     start_date = datetime.date(start_year, today.month, today.day)
 
-    path = os.path.join(parent, start_date.strftime('%Y-%m-%d'))
-    if not os.path.exists(path):
-        os.makedirs(path)
-            
+    today_folder = os.path.join(parent, start_date.strftime('%Y-%m-%d'))
+    if not os.path.exists(today_folder):
+        os.makedirs(today_folder)
+    loop = 0
+    while (loop ==0):
+        user_options = input("Please enter 1 to get stock data\n Please enter 2, to clean data\n Enter 3 to model data")
+        if user_options ==1:
+            indivdual_or_list = input("Enter 1 for indivdual stock or 2 for stock index:  ")
+            if indivdual_or_list ==1:
+                stock_list= input("Please enter the stock Symbol:  ")
 
-    # Download and clean stock data
+
+            else:
+                index_url = input("Please enter the url of the Index List:   ")
+                Complist = Get_SP500(url)
+                stock_list = Complist.download()
+            stocks = Get_Stock_History(today_folder, stock_list, start_date, today)
+            stocks.compstockdata()
+        elif user_options ==2:
+            pass
+        elif user_options ==3:
+            pass
+        else:
+            loop=1
+
     
-    #Complist = Get_SP500(url)
-    #sp500List = Complist.download()
-    sp500List = ['aapl']
-    sp500_list = Get_Stock_History(path, sp500List, start_datetime_object, end_date)
-    sp500_list.compstockdata()
 
-    
-
-    # Train model and make predictions
