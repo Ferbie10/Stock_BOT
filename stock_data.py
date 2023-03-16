bcc0e43bc9158319fc9208a30d800a568d402f4b
justin <j.j.uding.festus@gmail.com>
2023-03-16 16:34:16 +0000

    modified:   LSTM_Model.py 	modified:   stock_data.py

--- stock_data.py
+++ stock_data.py
@@ -43,11 +43,7 @@ class Get_Stock_History:
         for symbol in self.sp500:
             ticker = yf.Ticker(symbol)
             tik_history = ticker.history(
-<<<<<<< HEAD
                 period='15y', interval='1d')
-=======
-                period='1mo', interval='15m')
->>>>>>> 8724da693b9f77b24ed000565fef326afb159b14
             filename = os.path.join(self.path, f'{symbol}.csv')
             if not os.path.exists(filename):
                 tik_history.to_csv(filename)
@@ -91,11 +87,7 @@ def main():
     elif os.path.exists(today_folder):
         print(f"Path  {today_folder}")
     sp500 = ["aapl"]
-<<<<<<< HEAD
     test = Get_Stock_History(today_folder, sp500, start_date)
-=======
-    test = Get_Stock_History(today_folder, sp500, today_folder, start_date)
->>>>>>> 8724da693b9f77b24ed000565fef326afb159b14
     test.compstockdata()
 
 
